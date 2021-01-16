d0ba5650da926023d84e64e6917897cd14544e53
Hyunjin <hyunjin1109@korea.ac.kr>
2021-01-17 03:57:05 +0900

    Unit Class

---
+++ Vaccine/Assets/Scripts/LeukocyteUnit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeukocyteUnit : Unit
+{
+
+    bool Invincible = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        MaxHealth = Health = 350;
+    }
+
+    public override void GetDamaged(float damage)
+    {
+
+        if (!Invincible) base.GetDamaged(damage);
+        if (Health <= 0)
+            gameObject.SetActive(false);
+    }
+
+    protected override Queue<IEnumerator> DecideNextRoutine()
+    {
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
+
+        return nextRoutines;
+    }
+}
---
+++ Vaccine/Assets/Scripts/Unit.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+public abstract class Unit : MonoBehaviour
+{
+
+    protected Coroutine CurrentRoutine { get; private set; }
+    private Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
+
+    public float Health { get; protected set; }
+    public float MaxHealth { get; protected set; }
+
+    protected Animator animator;
+
+    protected Collider2D col;
+    protected Rigidbody2D rb;
+
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(CurrentRoutine == null)
+        {
+            NextRoutine();
+        }
+    }
+
+    public virtual void GetDamaged(float damage)
+    {
+        damage += UnityEngine.Random.Range(-1, 2);
+        Health -= damage;
+        if(Health < 0)
+        {
+
+        }
+    }
+
+
+    private void NextRoutine()
+    {
+        if (nextRoutines.Count <= 0)
+        {
+            nextRoutines = DecideNextRoutine();
+        }
+        StartCoroutineUnit(nextRoutines.Dequeue());
+    }
+
+    protected abstract Queue<IEnumerator> DecideNextRoutine();
+
+    private void StartCoroutineUnit(IEnumerator coroutine)
+    {
+        if (CurrentRoutine != null)
+        {
+            StopCoroutine(CurrentRoutine);
+        }
+        CurrentRoutine = StartCoroutine(coroutine);
+    }
+
+    protected IEnumerator NewActionRoutine(IEnumerator action)
+    {
+        yield return action;
+        CurrentRoutine = null;
+    }
+
+    protected IEnumerator MoveRoutine(Vector3 destination, float time)
+    {
+        yield return MoveRoutine(destination, time, AnimationCurve.Linear(0, 0, 1, 1));
+    }
+    protected IEnumerator MoveRoutine(Vector3 destination, float time, AnimationCurve curve)
+    {
+        Vector3 startPosition = transform.position;
+        Debug.Log(startPosition);
+        for (float t = 0; t <= time; t += Time.deltaTime)
+        {
+            transform.position =
+                Vector3.Lerp(startPosition, destination, curve.Evaluate(t / time));
+            yield return null;
+        }
+        transform.position = destination;
+    }
+
+    protected IEnumerator WaitRoutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+}
