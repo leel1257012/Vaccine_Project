8cb81a358a0163431e1c7f537d50a62b11527611
strn18 <strn18@korea.ac.kr>
2021-01-19 15:50:30 +0900

    Create Unit 2. Healthy wall(헬창 벽)

---
+++ Vaccine/Assets/Scripts/Units/HealthyWallUnit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthyWallUnit : Unit
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        MaxHealth = Health = 5;
+    }
+
+    public override void GetDamaged(float damage)
+    {
+
+        if (!Invincible) base.GetDamaged(damage);
+
+    }
+    protected override Queue<IEnumerator> DecideNextRoutine()
+    {
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
+
+        //nextRoutines.Enqueue(NewActionRoutine(Fire(range, interval)));
+
+        return nextRoutines;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
