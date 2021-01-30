11a63e3f8c8ad669fa56b06a145dc30be3d0d203
Hyunjin <hyunjin1109@korea.ac.kr>
2021-01-30 14:08:23 +0900

    Added StickyUnit (WIP)

---
+++ Vaccine/Assets/Scripts/Units/StickyUnit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyUnit : Unit
+{
+    // (temp) Place it in row3
+    protected override void Start()
+    {
+        base.Start();
+        MaxHealth = Health = 1;
+    }
+
+    public override void GetDamaged(float damage)
+    {
+
+        if (!Invincible) base.GetDamaged(damage);
+
+    }
+
+
+    protected override Queue<IEnumerator> DecideNextRoutine()
+    {
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
+
+        nextRoutines.Enqueue(NewActionRoutine(IdleRoutine(10f)));
+
+        return nextRoutines;
+    }
+
+    private IEnumerator IdleRoutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<VirusClass>() != null)
+        {
+            gameObject.transform.localScale += new Vector3(0f, 5f, 0f);
+        }
+    }
+
+}
