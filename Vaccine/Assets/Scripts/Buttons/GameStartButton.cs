9b39a8e8acad851939b533b4f25aabd1080fa22e
strn18 <strn18@korea.ac.kr>
2021-02-13 19:01:09 +0900

    Create MainScene, VaccineSeleceScene

---
+++ Vaccine/Assets/Scripts/Buttons/GameStartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartButton : MonoBehaviour
+{
+    public void LoadVaccineSelectScene()
+    {
+        SceneManager.LoadScene("VaccineSelectScene");
+    }
+}
